582a491fb03673e9edc87b07df93d70829c320a3
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2013-10-26 14:27:27 -0400

    Convert src/test/to_str_backend.cxx to new testing framework

--- Makefile
+++ Makefile
@@ -86,9 +86,11 @@ $(O)lib/libabc-testing$(LIBEXT): \
 
 # Test suite.
 $(O)bin/abc-test$(EXEEXT): \
+	$(O)obj/mock/iostream.cxx$(OBJEXT) \
 	$(O)obj/test/abc-test.cxx$(OBJEXT) \
 	$(O)obj/test/file_path.cxx$(OBJEXT) \
 	$(O)obj/test/str.cxx$(OBJEXT) \
+	$(O)obj/test/to_str_backend.cxx$(OBJEXT) \
 	| $(O)lib/libabc$(LIBEXT) \
 	  $(O)lib/libabc-testing$(LIBEXT)
 $(O)bin/abc-test$(EXEEXT): LDLIBS+=-labc-testing
--- src/test/to_str_backend.cxx
+++ src/test/to_str_backend.cxx
@@ -17,139 +17,21 @@ You should have received a copy of the GNU General Public License along with ABC
 <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------------------------*/
 
-#include <abc/module.hxx>
-#include <abc/mock/iostream.hxx>
+#include <abc/testing/unit.hxx>
 #include <abc/trace.hxx>
-using namespace abc;
+#include <abc/mock/iostream.hxx>
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// abc::test::to_str_backend_unit_base
 
-class test_app_module :
-	public app_module_impl<test_app_module> {
-public:
+namespace abc {
 
-	int main(mvector<istr const> const & vsArgs) {
-		abc_trace_fn((/*vsArgs*/));
+namespace test {
 
-		UNUSED_ARG(vsArgs);
-
-		// Test zero, decimal base.
-		if (!test_to_str_backend_output(0, SL(""), SL("0"))) {
-			return 10;
-		}
-		if (!test_to_str_backend_output(0, SL(" 1"), SL(" 0"))) {
-			return 11;
-		}
-		if (!test_to_str_backend_output(0, SL("01"), SL("0"))) {
-			return 12;
-		}
-		if (!test_to_str_backend_output(0, SL(" 2"), SL(" 0"))) {
-			return 13;
-		}
-		if (!test_to_str_backend_output(0, SL("02"), SL("00"))) {
-			return 14;
-		}
-
-		// Test positive values, decimal base.
-		if (!test_to_str_backend_output(1, SL(""), SL("1"))) {
-			return 20;
-		}
-		if (!test_to_str_backend_output(1, SL(" 1"), SL(" 1"))) {
-			return 21;
-		}
-		if (!test_to_str_backend_output(1, SL("01"), SL("1"))) {
-			return 22;
-		}
-		if (!test_to_str_backend_output(1, SL(" 2"), SL(" 1"))) {
-			return 23;
-		}
-		if (!test_to_str_backend_output(1, SL("02"), SL("01"))) {
-			return 24;
-		}
-
-		// Test negative values, decimal base.
-		if (!test_to_str_backend_output(-1, SL(""), SL("-1"))) {
-			return 30;
-		}
-		if (!test_to_str_backend_output(-1, SL(" 1"), SL("-1"))) {
-			return 31;
-		}
-		if (!test_to_str_backend_output(-1, SL("01"), SL("-1"))) {
-			return 32;
-		}
-		if (!test_to_str_backend_output(-1, SL(" 2"), SL("-1"))) {
-			return 33;
-		}
-		if (!test_to_str_backend_output(-1, SL("02"), SL("-1"))) {
-			return 34;
-		}
-		if (!test_to_str_backend_output(-1, SL(" 3"), SL(" -1"))) {
-			return 35;
-		}
-		if (!test_to_str_backend_output(-1, SL("03"), SL("-01"))) {
-			return 36;
-		}
-
-		// Test zero, hexadecimal base.
-		if (!test_to_str_backend_output(int8_t(0), SL("x"), SL("0"))) {
-			return 40;
-		}
-		if (!test_to_str_backend_output(int8_t(0), SL(" 1x"), SL("0"))) {
-			return 41;
-		}
-		if (!test_to_str_backend_output(int8_t(0), SL("01x"), SL("0"))) {
-			return 42;
-		}
-		if (!test_to_str_backend_output(int8_t(0), SL(" 2x"), SL(" 0"))) {
-			return 43;
-		}
-		if (!test_to_str_backend_output(int8_t(0), SL("02x"), SL("00"))) {
-			return 44;
-		}
-
-		// Test positive values, hexadecimal base.
-		if (!test_to_str_backend_output(int8_t(1), SL("x"), SL("1"))) {
-			return 50;
-		}
-		if (!test_to_str_backend_output(int8_t(1), SL(" 1x"), SL("1"))) {
-			return 51;
-		}
-		if (!test_to_str_backend_output(int8_t(1), SL("01x"), SL("1"))) {
-			return 52;
-		}
-		if (!test_to_str_backend_output(int8_t(1), SL(" 2x"), SL(" 1"))) {
-			return 53;
-		}
-		if (!test_to_str_backend_output(int8_t(1), SL("02x"), SL("01"))) {
-			return 54;
-		}
-
-		// Test negative values, hexadecimal base.
-		if (!test_to_str_backend_output(int8_t(-1), SL("x"), SL("ff"))) {
-			return 60;
-		}
-		if (!test_to_str_backend_output(int8_t(-1), SL(" 1x"), SL("ff"))) {
-			return 61;
-		}
-		if (!test_to_str_backend_output(int8_t(-1), SL("01x"), SL("ff"))) {
-			return 62;
-		}
-		if (!test_to_str_backend_output(int8_t(-1), SL(" 2x"), SL("ff"))) {
-			return 63;
-		}
-		if (!test_to_str_backend_output(int8_t(-1), SL("02x"), SL("ff"))) {
-			return 64;
-		}
-		if (!test_to_str_backend_output(int8_t(-1), SL(" 3x"), SL(" ff"))) {
-			return 65;
-		}
-		if (!test_to_str_backend_output(int8_t(-1), SL("03x"), SL("0ff"))) {
-			return 66;
-		}
-
-		return EXIT_SUCCESS;
-	}
-
+class to_str_backend_unit_base :
+	public testing::unit {
+protected:
 
 	/** Writes the first argument using the second as format specification, and compares the result
 	with the provided expectation, returning true if they match.
@@ -157,7 +39,7 @@ public:
 	TODO: comment signature.
 	*/
 	template <typename T, size_t t_cchFormatSpec, size_t t_cchExpected>
-	static bool test_to_str_backend_output(
+	static bool check_to_str_backend_output(
 		T const & t,
 		char_t const (& achFormatSpec)[t_cchFormatSpec],
 		char_t const (& achExpected)[t_cchExpected]
@@ -171,5 +53,120 @@ public:
 	}
 };
 
-ABC_MAIN_APP_MODULE(test_app_module)
+} //namespace test
+
+} //namespace abc
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// abc::test::to_str_backend_int
+
+namespace abc {
+
+namespace test {
+
+class to_str_backend_int :
+	public to_str_backend_unit_base {
+public:
+
+	/** See to_str_backend_unit_base::title().
+	*/
+	virtual istr title() {
+		return istr(SL("abc::to_str_backend - int"));
+	}
+
+
+	/** See to_str_backend_unit_base::run().
+	*/
+	virtual void run() {
+		abc_trace_fn((this));
+
+		// Test zero, decimal base.
+		ABC_TESTING_EXPECT(check_to_str_backend_output(0, SL(""), SL("0")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(0, SL(" 1"), SL(" 0")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(0, SL("01"), SL("0")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(0, SL(" 2"), SL(" 0")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(0, SL("02"), SL("00")));
+
+		// Test positive values, decimal base.
+		ABC_TESTING_EXPECT(check_to_str_backend_output(1, SL(""), SL("1")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(1, SL(" 1"), SL(" 1")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(1, SL("01"), SL("1")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(1, SL(" 2"), SL(" 1")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(1, SL("02"), SL("01")));
+
+		// Test negative values, decimal base.
+		ABC_TESTING_EXPECT(check_to_str_backend_output(-1, SL(""), SL("-1")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(-1, SL(" 1"), SL("-1")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(-1, SL("01"), SL("-1")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(-1, SL(" 2"), SL("-1")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(-1, SL("02"), SL("-1")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(-1, SL(" 3"), SL(" -1")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(-1, SL("03"), SL("-01")));
+	}
+};
+
+} //namespace test
+
+} //namespace abc
+
+ABC_TESTING_UNIT_REGISTER(abc::test::to_str_backend_int)
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// abc::test::to_str_backend_int8
+
+namespace abc {
+
+namespace test {
+
+class to_str_backend_int8 :
+	public to_str_backend_unit_base {
+public:
+
+	/** See to_str_backend_unit_base::title().
+	*/
+	virtual istr title() {
+		return istr(SL("abc::to_str_backend - int8_t"));
+	}
+
+
+	/** See to_str_backend_unit_base::run().
+	*/
+	virtual void run() {
+		abc_trace_fn((this));
+
+		// Test zero, hexadecimal base.
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(0), SL("x"), SL("0")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(0), SL(" 1x"), SL("0")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(0), SL("01x"), SL("0")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(0), SL(" 2x"), SL(" 0")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(0), SL("02x"), SL("00")));
+
+		// Test positive values, hexadecimal base.
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(1), SL("x"), SL("1")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(1), SL(" 1x"), SL("1")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(1), SL("01x"), SL("1")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(1), SL(" 2x"), SL(" 1")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(1), SL("02x"), SL("01")));
+
+		// Test negative values, hexadecimal base.
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(-1), SL("x"), SL("ff")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(-1), SL(" 1x"), SL("ff")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(-1), SL("01x"), SL("ff")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(-1), SL(" 2x"), SL("ff")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(-1), SL("02x"), SL("ff")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(-1), SL(" 3x"), SL(" ff")));
+		ABC_TESTING_EXPECT(check_to_str_backend_output(int8_t(-1), SL("03x"), SL("0ff")));
+	}
+};
+
+} //namespace test
+
+} //namespace abc
+
+ABC_TESTING_UNIT_REGISTER(abc::test::to_str_backend_int8)
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
