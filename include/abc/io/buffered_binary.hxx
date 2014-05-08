ca6479d3c41e6eecef2104d711cd06e4ee33d0f7
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2014-05-08 01:12:35 -0400

    Add buffered binary I/O classes

---
+++ include/abc/io/buffered_binary.hxx
@@ -0,0 +1,121 @@
+﻿/* -*- coding: utf-8; mode: c++; tab-width: 3; indent-tabs-mode: nil -*-
+
+Copyright 2014
+Raffaello D. Di Napoli
+
+This file is part of Application-Building Components (henceforth referred to as ABC).
+
+ABC is free software: you can redistribute it and/or modify it under the terms of the GNU General
+Public License as published by the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+ABC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+License for more details.
+
+You should have received a copy of the GNU General Public License along with ABC. If not, see
+<http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------------------------------*/
+
+#ifndef _ABC_IO_BUFFERED_BINARY_HXX
+#define _ABC_IO_BUFFERED_BINARY_HXX
+
+#include <abc/core.hxx>
+#ifdef ABC_CXX_PRAGMA_ONCE
+   #pragma once
+#endif
+
+#include <abc/io/binary.hxx>
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// abc::io::buffered_binary_reader
+
+
+namespace abc {
+
+namespace io {
+
+/** Provides buffering on top of a binary_reader instance.
+*/
+class ABCAPI buffered_binary_reader :
+   public virtual binary_reader {
+public:
+
+   /** See binary_reader::binary_reader().
+   */
+   buffered_binary_reader(std::shared_ptr<binary_reader> pbr);
+
+
+   /** Destructor.
+   */
+   virtual ~buffered_binary_reader();
+
+
+   /** See binary_reader::read().
+   */
+   virtual size_t read(void * p, size_t cbMax);
+
+
+protected:
+
+   /** Wrapped binary reader. */
+   std::shared_ptr<binary_reader> m_pbr;
+};
+
+} //namespace io
+
+} //namespace abc
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// abc::io::buffered_binary_writer
+
+
+namespace abc {
+
+namespace io {
+
+/** Provides buffering on top of a binary_writer instance.
+*/
+class ABCAPI buffered_binary_writer :
+   public virtual binary_writer {
+public:
+
+   /** See binary_writer::binary_writer().
+   */
+   buffered_binary_writer(std::shared_ptr<binary_writer> pbw);
+
+
+   /** Destructor.
+   */
+   virtual ~buffered_binary_writer();
+
+
+   /** See binary_writer::flush().
+   */
+   virtual void flush();
+
+
+   /** See binary_writer::write().
+   */
+   virtual size_t write(void const * p, size_t cb);
+
+
+protected:
+
+   /** Wrapped binary writer. */
+   std::shared_ptr<binary_writer> m_pbw;
+};
+
+} //namespace io
+
+} //namespace abc
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#endif //ifndef _ABC_IO_BUFFERED_BINARY_HXX
+
---
+++ src/abc/io/buffered_binary.cxx
@@ -0,0 +1,100 @@
+﻿/* -*- coding: utf-8; mode: c++; tab-width: 3; indent-tabs-mode: nil -*-
+
+Copyright 2014
+Raffaello D. Di Napoli
+
+This file is part of Application-Building Components (henceforth referred to as ABC).
+
+ABC is free software: you can redistribute it and/or modify it under the terms of the GNU General
+Public License as published by the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+ABC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+License for more details.
+
+You should have received a copy of the GNU General Public License along with ABC. If not, see
+<http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------------------------------*/
+
+#include <abc/core.hxx>
+#include <abc/io/buffered_binary.hxx>
+#include <abc/numeric.hxx>
+#include <abc/trace.hxx>
+#include <algorithm>
+#if ABC_HOST_API_POSIX
+   #include <unistd.h> // *_FILENO ssize_t close() isatty() open() read() write()
+   #include <fcntl.h> // O_*
+   #include <sys/stat.h> // S_*, stat()
+// #include <sys/mman.h> // mmap(), munmap(), PROT_*, MAP_*
+#endif
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// abc::io::buffered_binary_reader
+
+
+namespace abc {
+
+namespace io {
+
+buffered_binary_reader::buffered_binary_reader(std::shared_ptr<binary_reader> pbr) :
+   binary_reader(),
+   m_pbr(std::move(pbr)) {
+}
+
+
+/*virtual*/ buffered_binary_reader::~buffered_binary_reader() {
+}
+
+
+/*virtual*/ size_t buffered_binary_reader::read(void * p, size_t cbMax) {
+   ABC_TRACE_FN((this, p, cbMax));
+
+   return m_pbr->read(p, cbMax);
+}
+
+} //namespace io
+
+} //namespace abc
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// abc::io::buffered_binary_writer
+
+
+namespace abc {
+
+namespace io {
+
+buffered_binary_writer::buffered_binary_writer(std::shared_ptr<binary_writer> pbw) :
+   binary_writer(),
+   m_pbw(std::move(pbw)) {
+}
+
+
+/*virtual*/ buffered_binary_writer::~buffered_binary_writer() {
+}
+
+
+/*virtual*/ void buffered_binary_writer::flush() {
+   ABC_TRACE_FN((this));
+
+   m_pbw->flush();
+}
+
+
+/*virtual*/ size_t buffered_binary_writer::write(void const * p, size_t cb) {
+   ABC_TRACE_FN((this, p, cb));
+
+   return m_pbw->write(p, cb);
+}
+
+} //namespace io
+
+} //namespace abc
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
