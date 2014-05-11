4abffa3b41dd50b1e610d2fee6bf4e749af4b838
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2014-05-11 12:02:22 -0400

    Remove incorrect header file inclusion

--- src/abc-test-cppmacros/cppmacros.cxx
+++ src/abc-test-cppmacros/cppmacros.cxx
@@ -18,7 +18,6 @@ You should have received a copy of the GNU General Public License along with ABC
 --------------------------------------------------------------------------------------------------*/
 
 #include <abc/core.hxx>
-#include <abc/cppmacros.hxx>
 
 ABCMK_CMP_BEGIN
 
