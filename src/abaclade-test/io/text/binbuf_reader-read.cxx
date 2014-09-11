77f9f14edbf9f5dda737c29a07a2ebeea13b0964
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2014-09-11 07:53:28 -0400

    De-duplicate code in unit tests

--- src/abaclade-test/io/text/binbuf_reader-read.cxx
+++ src/abaclade-test/io/text/binbuf_reader-read.cxx
@@ -24,34 +24,59 @@ You should have received a copy of the GNU General Public License along with Aba
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+// abc::test::binbuf_reader_read_line_base
+
+namespace abc {
+namespace test {
+
+class binbuf_reader_read_line_base :
+   public testing::test_case {
+public:
+
+   /*! Returns the path to the test data.
+
+   return
+      Relative path to the test data file.
+   */
+   virtual file_path get_test_data() = 0;
+
+   //! See testing::test_case::run().
+   virtual void run() /*override*/ {
+      ABC_TRACE_FUNC(this);
+
+      auto ptrIn(io::text::open_reader(get_test_data()));
+      dmstr sLine;
+      unsigned i(1);
+      while (ptrIn->read_line(&sLine)) {
+         ABC_TESTING_ASSERT_EQUAL(sLine.size(), i++);
+      }
+   }
+};
+
+} //namespace test
+} //namespace abc
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 // abc::test::binbuf_reader_read_line_utf8_lf_no_trailing_nl
 
 namespace abc {
 namespace test {
 
 class binbuf_reader_read_line_utf8_lf_no_trailing_nl :
-   public testing::test_case {
+   public binbuf_reader_read_line_base {
 public:
 
-   //! See testing::test_case::title().
-   virtual istr title() {
+   //! See binbuf_reader_read_line_base::title().
+   virtual istr title() /*override*/ {
       return istr(ABC_SL(
          "abc::io::text::binbuf_reader – reading line-by-line, UTF-8, LF, no trailing LF"
       ));
    }
 
-   //! See testing::test_case::run().
-   virtual void run() {
-      ABC_TRACE_FUNC(this);
-
-      auto ptrIn(io::text::open_reader(
-         file_path(ABC_SL("src/abaclade-test/io/text/data/utf8_lf_no-trailing-nl.txt"))
-      ));
-      dmstr sLine;
-      unsigned i(1);
-      while (ptrIn->read_line(&sLine)) {
-         ABC_TESTING_ASSERT_EQUAL(sLine.size(), i++);
-      }
+   //! See binbuf_reader_read_line_base::get_test_data().
+   virtual file_path get_test_data() /*override*/ {
+      return file_path(ABC_SL("src/abaclade-test/io/text/data/utf8_lf_no-trailing-nl.txt"));
    }
 };
 
@@ -68,29 +93,20 @@ namespace abc {
 namespace test {
 
 class binbuf_reader_read_line_utf8_mixed_no_trailing_nl :
-   public testing::test_case {
+   public binbuf_reader_read_line_base {
 public:
 
-   //! See testing::test_case::title().
-   virtual istr title() {
+   //! See binbuf_reader_read_line_base::title().
+   virtual istr title() /*override*/ {
       return istr(ABC_SL(
          "abc::io::text::binbuf_reader – "
             "reading line-by-line, UTF-8, CR/LF/CRLF mix, no trailing LF"
       ));
    }
 
-   //! See testing::test_case::run().
-   virtual void run() {
-      ABC_TRACE_FUNC(this);
-
-      auto ptrIn(io::text::open_reader(
-         file_path(ABC_SL("src/abaclade-test/io/text/data/utf8_mixed_no-trailing-nl.txt"))
-      ));
-      dmstr sLine;
-      unsigned i(1);
-      while (ptrIn->read_line(&sLine)) {
-         ABC_TESTING_ASSERT_EQUAL(sLine.size(), i++);
-      }
+   //! See binbuf_reader_read_line_base::get_test_data().
+   virtual file_path get_test_data() /*override*/ {
+      return file_path(ABC_SL("src/abaclade-test/io/text/data/utf8_mixed_no-trailing-nl.txt"));
    }
 };
 
@@ -107,28 +123,19 @@ namespace abc {
 namespace test {
 
 class binbuf_reader_read_line_utf32_lf_no_trailing_nl :
-   public testing::test_case {
+   public binbuf_reader_read_line_base {
 public:
 
-   //! See testing::test_case::title().
-   virtual istr title() {
+   //! See binbuf_reader_read_line_base::title().
+   virtual istr title() /*override*/ {
       return istr(ABC_SL(
          "abc::io::text::binbuf_reader – reading line-by-line, UTF-32, LF, no trailing LF"
       ));
    }
 
-   //! See testing::test_case::run().
-   virtual void run() {
-      ABC_TRACE_FUNC(this);
-
-      auto ptrIn(io::text::open_reader(
-         file_path(ABC_SL("src/abaclade-test/io/text/data/utf32le+bom_lf_no-trailing-nl.txt"))
-      ));
-      dmstr sLine;
-      unsigned i(1);
-      while (ptrIn->read_line(&sLine)) {
-         ABC_TESTING_ASSERT_EQUAL(sLine.size(), i++);
-      }
+   //! See binbuf_reader_read_line_base::get_test_data().
+   virtual file_path get_test_data() /*override*/ {
+      return file_path(ABC_SL("src/abaclade-test/io/text/data/utf32le+bom_lf_no-trailing-nl.txt"));
    }
 };
 
