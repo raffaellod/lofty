31d801a36b12622ccfafce191284c23c179536c4
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2014-12-22 14:06:55 -0500

    One more missing #include

--- src/abaclade/io/text.cxx
+++ src/abaclade/io/text.cxx
@@ -20,6 +20,8 @@ You should have received a copy of the GNU General Public License along with Aba
 #include <abaclade.hxx>
 #include <abaclade/io/binary/file.hxx>
 
+#include <algorithm>
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // abc::io::text::base
