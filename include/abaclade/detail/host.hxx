c592dd978412c5e81c15c6e2478d310486606ffc
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2015-01-24 23:15:54 -0500

    Add support for ARM target

--- include/abaclade/detail/host.hxx
+++ include/abaclade/detail/host.hxx
@@ -107,6 +107,7 @@ You should have received a copy of the GNU General Public License along with Aba
 // abc globals – ABC_HOST_ARCH_*
 
 #define ABC_HOST_ARCH_ALPHA  0
+#define ABC_HOST_ARCH_ARM    0
 #define ABC_HOST_ARCH_I386   0
 #define ABC_HOST_ARCH_IA64   0
 #define ABC_HOST_ARCH_PPC    0
@@ -115,16 +116,19 @@ You should have received a copy of the GNU General Public License along with Aba
 #if defined(__alpha__) || defined(_M_ALPHA)
    #undef ABC_HOST_ARCH_ALPHA
    #define ABC_HOST_ARCH_ALPHA 1
+#elif defined(__arm__) || defined(_M_ARM)
+   #undef ABC_HOST_ARCH_ARM
+   #define ABC_HOST_ARCH_ARM 1
 #elif defined(__i386__) || defined(_M_IX86)
    #undef ABC_HOST_ARCH_I386
    #define ABC_HOST_ARCH_I386 1
 #elif defined(__ia64__) || defined(_M_IA64)
    #undef ABC_HOST_ARCH_IA64
    #define ABC_HOST_ARCH_IA64 1
-#elif defined(__powerpc__) || defined(_M_PPC) || defined(_M_MPPC)
+#elif defined(__powerpc__) || defined(_M_PPC)
    #undef ABC_HOST_ARCH_PPC
    #define ABC_HOST_ARCH_PPC 1
-#elif defined(__x86_64__) || defined(_M_X64) || defined(_M_AMD64)
+#elif defined(__x86_64__) || defined(__amd64__) || defined(_M_X64) || defined(_M_AMD64)
    #undef ABC_HOST_ARCH_X86_64
    #define ABC_HOST_ARCH_X86_64 1
 #else
@@ -152,7 +156,9 @@ You should have received a copy of the GNU General Public License along with Aba
 #define ABC_HOST_LITTLE_ENDIAN 0
 #define ABC_HOST_BIG_ENDIAN    0
 
-#if ABC_HOST_ARCH_ALPHA || ABC_HOST_ARCH_I386 || ABC_HOST_ARCH_IA64 || ABC_HOST_ARCH_X86_64
+// Assume that ARM is always used in little-endian mode.
+#if ABC_HOST_ARCH_ALPHA || ABC_HOST_ARCH_ARM || ABC_HOST_ARCH_I386 || ABC_HOST_ARCH_IA64 || \
+      ABC_HOST_ARCH_X86_64
    #undef ABC_HOST_LITTLE_ENDIAN
    #define ABC_HOST_LITTLE_ENDIAN 1
 #elif ABC_HOST_ARCH_PPC
