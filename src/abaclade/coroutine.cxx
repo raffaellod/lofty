10b5c193f7cf0ce8bf9d5f3f01d19232a7e63336
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2015-03-09 15:54:45 -0400

    Initial (non-working) coroutines implementation for *BSD/OS X

--- src/abaclade/coroutine.cxx
+++ src/abaclade/coroutine.cxx
@@ -22,13 +22,16 @@ You should have received a copy of the GNU General Public License along with Aba
 
 #if ABC_HOST_API_POSIX
    #include <errno.h> // EINTR errno
+   #if ABC_HOST_API_DARWIN
+      #define _XOPEN_SOURCE
+   #endif
+   #include <ucontext.h>
    #if ABC_HOST_API_BSD
       #include <sys/event.h>
       #include <sys/time.h>
       #include <sys/types.h>
    #elif ABC_HOST_API_LINUX
       #include <sys/epoll.h>
-      #include <ucontext.h>
    #endif
 #endif
 
@@ -44,6 +47,7 @@ public:
       m_fnInnerMain(std::move(fnMain)) {
    }
 
+#if ABC_HOST_API_BSD
    void reset(::ucontext_t * puctxReturn) {
       if (::getcontext(&m_uctx) < 0) {
          exception::throw_os_error();
@@ -53,6 +57,21 @@ public:
       m_uctx.uc_link = puctxReturn;
       ::makecontext(&m_uctx, reinterpret_cast<void (*)()>(&outer_main), 1, this);
    }
+#elif ABC_HOST_API_LINUX //if ABC_HOST_API_BSD
+   void reset(::ucontext_t * puctxReturn) {
+      if (::getcontext(&m_uctx) < 0) {
+         exception::throw_os_error();
+      }
+      m_uctx.uc_stack.ss_sp = &m_aiStack;
+      m_uctx.uc_stack.ss_size = sizeof m_aiStack;
+      m_uctx.uc_link = puctxReturn;
+      ::makecontext(&m_uctx, reinterpret_cast<void (*)()>(&outer_main), 1, this);
+   }
+#elif ABC_HOST_API_WIN32 //if ABC_HOST_API_BSD … elif ABC_HOST_API_LINUX
+   #error "TODO: HOST_API"
+#else //if ABC_HOST_API_BSD … elif ABC_HOST_API_LINUX … elif ABC_HOST_API_WIN32
+   #error "TODO: HOST_API"
+#endif //if ABC_HOST_API_BSD … elif ABC_HOST_API_LINUX … elif ABC_HOST_API_WIN32 … else
 
 private:
    static void outer_main(void * p) {
@@ -68,12 +87,25 @@ private:
       }
    }
 
+private:
+#if ABC_HOST_API_BSD
 public:
    ::ucontext_t m_uctx;
 private:
-   std::function<void ()> m_fnInnerMain;
    // TODO: use MINSIGSTKSZ.
    abc::max_align_t m_aiStack[1024];
+#elif ABC_HOST_API_LINUX //if ABC_HOST_API_BSD
+public:
+   ::ucontext_t m_uctx;
+private:
+   // TODO: use MINSIGSTKSZ.
+   abc::max_align_t m_aiStack[1024];
+#elif ABC_HOST_API_WIN32 //if ABC_HOST_API_BSD … elif ABC_HOST_API_LINUX
+   #error "TODO: HOST_API"
+#else //if ABC_HOST_API_BSD … elif ABC_HOST_API_LINUX … elif ABC_HOST_API_WIN32
+   #error "TODO: HOST_API"
+#endif //if ABC_HOST_API_BSD … elif ABC_HOST_API_LINUX … elif ABC_HOST_API_WIN32 … else
+   std::function<void ()> m_fnInnerMain;
 };
 
 
@@ -92,6 +124,116 @@ coroutine::~coroutine() {
 // abc::coroutine_scheduler
 
 namespace abc {
+
+#if ABC_HOST_API_BSD
+
+class coroutine_scheduler_impl : public coroutine_scheduler {
+public:
+   //! Constructor.
+   coroutine_scheduler_impl() :
+      m_fdKqueue(::kqueue()) {
+      if (!m_fdKqueue) {
+         exception::throw_os_error();
+      }
+      // TODO: set close-on-exec. ::kqueue1() may be available on some systems; investigate that.
+   }
+
+   //! Destructor.
+   virtual ~coroutine_scheduler_impl() {
+      // TODO: verify that m_listStartingCoros and m_mapBlockedCoros are empty.
+   }
+
+   //! See coroutine_scheduler::run().
+   virtual void run() override {
+      while ((m_pcoroctxActive = find_coroutine_to_activate())) {
+         if (::swapcontext(&m_uctxReturn, &m_pcoroctxActive->m_uctx) < 0) {
+            /* TODO: in case of errors, which should never happen here since all coroutines have the
+            same stack size, inject a stack overflow exception in *m_pcoroctxActive. */
+         }
+      }
+      // Release the last coroutine.
+      m_pcoroctxActive.reset();
+   }
+
+   //! See coroutine_scheduler::yield_while_async_pending().
+   virtual void yield_while_async_pending(io::filedesc const & fd, bool bWrite) override {
+      // Add fd to the epoll as a new event source.
+      struct ::kevent ke;
+      ke.ident = fd.get();
+      // Use EV_ONESHOT to avoid waking up multiple threads for the same fd becoming ready.
+      ke.flags = EV_ADD | EV_ONESHOT | EV_EOF ;
+      ke.filter = bWrite ? EVFILT_WRITE : EVFILT_READ;
+      if (::kevent(m_fdKqueue.get(), &ke, 1, nullptr, 0, nullptr) < 0) {
+         exception::throw_os_error();
+      }
+      // Deactivate the current coroutine and find one to activate instead.
+      coroutine::context * pcoroctxActive = m_pcoroctxActive.get();
+      auto itBlockedCoro(
+         m_mapBlockedCoros.add_or_assign(fd.get(), std::move(m_pcoroctxActive)).first
+      );
+      try {
+         m_pcoroctxActive = find_coroutine_to_activate();
+      } catch (...) {
+         // If anything went wrong, restore the coroutine that was active.
+         // TODO: need map::pop().
+         m_pcoroctxActive = std::move(itBlockedCoro->value);
+         m_mapBlockedCoros.remove(itBlockedCoro);
+         throw;
+      }
+      /* If the context changed, i.e. the coroutine that’s ready to run is not the one that was
+      active, switch to it. */
+      if (m_pcoroctxActive.get() != pcoroctxActive) {
+         if (::swapcontext(&pcoroctxActive->m_uctx, &m_pcoroctxActive->m_uctx) < 0) {
+            /* TODO: in case of errors, which should never happen here since all coroutines have the
+            same stack size, inject a stack overflow exception in *m_pcoroctxActive. */
+         }
+      }
+   }
+
+private:
+   std::shared_ptr<coroutine::context> find_coroutine_to_activate() {
+      // This loop will only repeat in case of EINTR during ::epoll_wait().
+      for (;;) {
+         if (m_listStartingCoros) {
+            // There are coroutines that haven’t had a chance to run; remove and schedule the first.
+            auto pcoroctx(m_listStartingCoros.pop_front());
+            // TODO: verify how this behaves in a multithreaded scenario.
+            pcoroctx->reset(&m_uctxReturn);
+            return std::move(pcoroctx);
+         } else if (m_mapBlockedCoros) {
+            // There are blocked coroutines; wait for the first one to become ready again.
+            struct ::kevent ke;
+            int cReadyEvents = ::kevent(m_fdKqueue.get(), nullptr, 0, &ke, 1, nullptr);
+            // 0 won’t really be returned; possible values are either 1 or -1.
+            if (cReadyEvents < 0) {
+               int iErr = errno;
+               if (iErr == EINTR) {
+                  continue;
+               }
+               exception::throw_os_error(iErr);
+            }
+            // Find which coroutine was waiting for eeReady and remove it from m_mapBlockedCoros.
+            // TODO: need map::pop().
+            auto itBlockedCoro(m_mapBlockedCoros.find(static_cast<int>(ke.ident)));
+            std::shared_ptr<coroutine::context> pcoroctxToActivate(std::move(itBlockedCoro->value));
+            m_mapBlockedCoros.remove(itBlockedCoro);
+            return std::move(pcoroctxToActivate);
+         } else {
+            return nullptr;
+         }
+      }
+   }
+
+private:
+   //! File descriptor of the internal epoll.
+   io::filedesc m_fdKqueue;
+   //! Coroutines that are blocked on a fd wait.
+   collections::map<int, std::shared_ptr<coroutine::context>> m_mapBlockedCoros;
+   // Coroutine context that every coroutine eventually returns to.
+   ::ucontext_t m_uctxReturn;
+};
+
+#elif ABC_HOST_API_LINUX //if ABC_HOST_API_BSD
 
 class coroutine_scheduler_impl : public coroutine_scheduler {
 public:
@@ -129,13 +271,13 @@ public:
       means we’d need to then rearm it in find_coroutine_to_activate() when it becomes ready, but
       we’ll remove it instead. */
       ee.events = EPOLLONESHOT | (bWrite ? EPOLLOUT : EPOLLIN) | EPOLLPRI;
-      if (::epoll_ctl(m_fdEpoll.get(), EPOLL_CTL_ADD, ee.data.fd, &ee) < 0) {
+      if (::epoll_ctl(m_fdEpoll.get(), EPOLL_CTL_ADD, fd.get(), &ee) < 0) {
          exception::throw_os_error();
       }
       // Deactivate the current coroutine and find one to activate instead.
       coroutine::context * pcoroctxActive = m_pcoroctxActive.get();
       auto itBlockedCoro(
-         m_mapBlockedCoros.add_or_assign(ee.data.fd, std::move(m_pcoroctxActive)).first
+         m_mapBlockedCoros.add_or_assign(fd.get(), std::move(m_pcoroctxActive)).first
       );
       try {
          m_pcoroctxActive = find_coroutine_to_activate();
@@ -202,6 +344,11 @@ private:
    ::ucontext_t m_uctxReturn;
 };
 
+#elif ABC_HOST_API_WIN32 //if ABC_HOST_API_BSD … elif ABC_HOST_API_LINUX
+   #error "TODO: HOST_API"
+#else //if ABC_HOST_API_BSD … elif ABC_HOST_API_LINUX … elif ABC_HOST_API_WIN32
+   #error "TODO: HOST_API"
+#endif //if ABC_HOST_API_BSD … elif ABC_HOST_API_LINUX … elif ABC_HOST_API_WIN32 … else
 
 thread_local_value<std::shared_ptr<coroutine_scheduler>> coroutine_scheduler::sm_pcorosched;
 
