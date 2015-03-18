1faa1e5f33053dfa70dd4a317affc94798710e25
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2015-03-18 08:22:50 -0400

    Introduce abc::net::tcp_server stub

---
+++ include/abaclade/net.hxx
@@ -0,0 +1,66 @@
+﻿/* -*- coding: utf-8; mode: c++; tab-width: 3; indent-tabs-mode: nil -*-
+
+Copyright 2015
+Raffaello D. Di Napoli
+
+This file is part of Abaclade.
+
+Abaclade is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+Abaclade is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Abaclade. If not, see
+<http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------------------------------*/
+
+#ifndef _ABACLADE_NET_HXX
+#define _ABACLADE_NET_HXX
+
+#ifndef _ABACLADE_HXX
+   #error "Please #include <abaclade.hxx> before this file"
+#endif
+#ifdef ABC_CXX_PRAGMA_ONCE
+   #pragma once
+#endif
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// abc::net::tcp_server
+
+namespace abc {
+namespace net {
+
+/*! Attaches a coroutine scheduler to the current thread, and performs and necessary initialization
+required for the current thread to run coroutines.
+*/
+class ABACLADE_SYM tcp_server : public noncopyable {
+public:
+   /*! Constructor.
+
+   @param sAddress
+      Address to bind to.
+   @param iPort
+      Port to listen for connections on.
+   @param cBacklog
+      Count of established connections that will be allowed to queue until the server is able to
+      accept them.
+   */
+   tcp_server(istr const & sAddress, std::uint16_t iPort, unsigned cBacklog = 5);
+
+   //! Destructor.
+   ~tcp_server();
+
+private:
+   io::filedesc m_fdSocket;
+};
+
+} //namespace net
+} //namespace abc
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#endif //ifndef _ABACLADE_NET_HXX
---
+++ src/abaclade/net.cxx
@@ -0,0 +1,72 @@
+﻿/* -*- coding: utf-8; mode: c++; tab-width: 3; indent-tabs-mode: nil -*-
+
+Copyright 2015
+Raffaello D. Di Napoli
+
+This file is part of Abaclade.
+
+Abaclade is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+Abaclade is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Abaclade. If not, see
+<http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------------------------------*/
+
+#include <abaclade.hxx>
+#include <abaclade/net.hxx>
+
+#if ABC_HOST_API_POSIX
+   #include <arpa/inet.h> // inet_addr()
+   #include <sys/types.h> // sockaddr sockaddr_in
+   #include <sys/socket.h> // accept() bind() socket()
+#endif
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// abc::net::tcp_server
+
+namespace abc {
+namespace net {
+
+tcp_server::tcp_server(istr const & sAddress, std::uint16_t iPort, unsigned cBacklog /*= 5*/) :
+   m_fdSocket(::socket(AF_INET, SOCK_STREAM, 0)) {
+   ABC_TRACE_FUNC(sAddress, iPort, cBacklog);
+
+   if (!m_fdSocket) {
+      exception::throw_os_error();
+   }
+
+   ::sockaddr_in saServer;
+   memory::clear(&saServer);
+   saServer.sin_family = AF_INET;
+   if (sAddress == ABC_SL("*")) {
+      saServer.sin_addr.s_addr = INADDR_ANY;
+   } else {
+      saServer.sin_addr.s_addr = ::inet_addr(sAddress.c_str());
+      if (saServer.sin_addr.s_addr == ::in_addr_t(-1)) {
+         // TODO: FIXME: ::inet_addr() doesn’t set errno!
+         exception::throw_os_error();
+      }
+   }
+   saServer.sin_port = ::htons(iPort);
+   if (::bind(m_fdSocket.get(), reinterpret_cast< ::sockaddr *>(&saServer), sizeof saServer) < 0) {
+      exception::throw_os_error();
+   }
+
+   if (::listen(m_fdSocket.get(), static_cast<int>(cBacklog)) < 0) {
+      exception::throw_os_error();
+   }
+}
+
+tcp_server::~tcp_server() {
+}
+
+} //namespace net
+} //namespace abc
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
