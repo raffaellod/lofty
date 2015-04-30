751b27094ce3fe942713ba1747988dfd741e1137
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2015-04-29 21:55:44 -0400

    Add test case for abc::defer_to_scope_end()

---
+++ test/abaclade/defer_to_scope_end.cxx
@@ -0,0 +1,44 @@
+﻿/* -*- coding: utf-8; mode: c++; tab-width: 3; indent-tabs-mode: nil -*-
+
+Copyright 2015
+Raffaello D. Di Napoli
+
+This file is part of Abaclade.
+
+Abaclade is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+Abaclade is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Abaclade. If not, see
+<http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------------------------------*/
+
+#include <abaclade.hxx>
+#include <abaclade/testing/test_case.hxx>
+#include <abaclade/defer_to_scope_end.hxx>
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// abc::test::defer_to_scope_end
+
+namespace abc {
+namespace test {
+
+ABC_TESTING_TEST_CASE_FUNC("abc::defer_to_scope_end – basic operation") {
+   ABC_TRACE_FUNC(this);
+
+   unsigned cDeferredInvocations = 0;
+   {
+      auto deferred1(defer_to_scope_end([&cDeferredInvocations] () -> void {
+         ++cDeferredInvocations;
+      }));
+   }
+   ABC_TESTING_ASSERT_EQUAL(cDeferredInvocations, 1u);
+}
+
+} //namespace test
+} //namespace abc
