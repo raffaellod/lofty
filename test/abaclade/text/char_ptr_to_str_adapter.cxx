16c6fa5d35696764b70f5c93522a0db3db05530b
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2015-06-15 22:04:34 -0400

    Add test case for abc::to_str_backend<abc::text::char_ptr_to_str_adapter>

---
+++ test/abaclade/text/char_ptr_to_str_adapter.cxx
@@ -0,0 +1,42 @@
+﻿/* -*- coding: utf-8; mode: c++; tab-width: 3; indent-tabs-mode: nil -*-
+
+Copyright 2015
+Raffaello D. Di Napoli
+
+This file is part of Abaclade.
+
+Abaclade is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+Abaclade is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Abaclade. If not, see
+<http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------------------------------*/
+
+#include <abaclade.hxx>
+#include <abaclade/testing/test_case.hxx>
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// abc::test::char_ptr_to_str_adapter
+
+namespace abc {
+namespace test {
+
+ABC_TESTING_TEST_CASE_FUNC("abc::to_str – abc::text::char_ptr_to_str_adapter") {
+   ABC_TRACE_FUNC(this);
+
+   ABC_TESTING_ASSERT_EQUAL(to_str(text::char_ptr_to_str_adapter(nullptr)), ABC_SL("<nullptr>"));
+   ABC_TESTING_ASSERT_EQUAL(to_str(text::char_ptr_to_str_adapter("")), ABC_SL(""));
+   ABC_TESTING_ASSERT_EQUAL(to_str(text::char_ptr_to_str_adapter("a")), ABC_SL("a"));
+   ABC_TESTING_ASSERT_EQUAL(to_str(text::char_ptr_to_str_adapter("ab")), ABC_SL("ab"));
+   ABC_TESTING_ASSERT_EQUAL(to_str(text::char_ptr_to_str_adapter("abc")), ABC_SL("abc"));
+   ABC_TESTING_ASSERT_EQUAL(to_str(text::char_ptr_to_str_adapter("ab\0c")), ABC_SL("ab"));
+}
+
+} //namespace test
+} //namespace abc
