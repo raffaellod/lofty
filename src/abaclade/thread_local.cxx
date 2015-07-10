7bc3c48dcead8da61f43acf5332ea43dc2ae31fe
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2015-07-10 07:42:12 -0400

    Add missing #include

--- src/abaclade/thread_local.cxx
+++ src/abaclade/thread_local.cxx
@@ -20,6 +20,8 @@ You should have received a copy of the GNU General Public License along with Aba
 #include <abaclade.hxx>
 #include <abaclade/bitmanip.hxx>
 
+#include <cstdlib> // std::abort()
+
 #if ABC_HOST_API_POSIX
    #include <pthread.h>
 #endif
