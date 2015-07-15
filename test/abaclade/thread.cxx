92a8d5bd0ac2c38fd453e93bffc743cd6ff316fa
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2015-07-14 21:19:59 -0400

    Disable thread interruption tests under FreeBSD

--- test/abaclade/thread.cxx
+++ test/abaclade/thread.cxx
@@ -77,7 +77,7 @@ ABC_TESTING_TEST_CASE_FUNC("abc::thread – concurrent operation") {
 
 namespace abc { namespace test {
 
-#if !ABC_HOST_API_MACH
+#if !ABC_HOST_API_FREEBSD && !ABC_HOST_API_MACH
 ABC_TESTING_TEST_CASE_FUNC("abc::thread – interruption") {
    ABC_TRACE_FUNC(this);
 
@@ -122,7 +122,7 @@ ABC_TESTING_TEST_CASE_FUNC("abc::thread – interruption") {
    ABC_TESTING_ASSERT_TRUE(abWorkersCompleted[4]);
    ABC_TESTING_ASSERT_FALSE(abWorkersInterrupted[4]);
 }
-#endif //if !ABC_HOST_API_MACH
+#endif //if !ABC_HOST_API_FREEBSD && !ABC_HOST_API_MACH
 
 }} //namespace abc::test
 
@@ -130,7 +130,7 @@ ABC_TESTING_TEST_CASE_FUNC("abc::thread – interruption") {
 
 namespace abc { namespace test {
 
-#if !ABC_HOST_API_MACH
+#if !ABC_HOST_API_FREEBSD && !ABC_HOST_API_MACH
 ABC_TESTING_TEST_CASE_FUNC("abc::thread – exception propagation") {
    ABC_TRACE_FUNC(this);
 
@@ -173,7 +173,7 @@ ABC_TESTING_TEST_CASE_FUNC("abc::thread – exception propagation") {
    // While we’re at it, verify that something was written to stderr while *ptswErr was stderr.
    ABC_TESTING_ASSERT_NOT_EQUAL(ptswErr->get_str(), istr::empty);
 }
-#endif //if !ABC_HOST_API_MACH
+#endif //if !ABC_HOST_API_FREEBSD && !ABC_HOST_API_MACH
 
 }} //namespace abc::test
 
@@ -181,7 +181,7 @@ ABC_TESTING_TEST_CASE_FUNC("abc::thread – exception propagation") {
 
 namespace abc { namespace test {
 
-#if !ABC_HOST_API_MACH
+#if !ABC_HOST_API_FREEBSD && !ABC_HOST_API_MACH
 ABC_TESTING_TEST_CASE_FUNC("abc::thread – interruption exception propagation") {
    ABC_TRACE_FUNC(this);
 
@@ -229,6 +229,6 @@ ABC_TESTING_TEST_CASE_FUNC("abc::thread – interruption exception propagation")
    // While we’re at it, verify that something was written to stderr while *ptswErr was stderr.
    ABC_TESTING_ASSERT_NOT_EQUAL(ptswErr->get_str(), istr::empty);
 }
-#endif //if !ABC_HOST_API_MACH
+#endif //if !ABC_HOST_API_FREEBSD && !ABC_HOST_API_MACH
 
 }} //namespace abc::test
