dcdf891606571f0b85b8c769213dda199e95440f
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2015-09-19 10:28:20 -0400

    Relicense Abaclade under GNU LGPLv3

--- include/abaclade/_std/typeinfo.hxx
+++ include/abaclade/_std/typeinfo.hxx
@@ -1,20 +1,19 @@
 ﻿/* -*- coding: utf-8; mode: c++; tab-width: 3; indent-tabs-mode: nil -*-
 
-Copyright 2011, 2012, 2013, 2014, 2015
-Raffaello D. Di Napoli
+Copyright 2011-2015 Raffaello D. Di Napoli
 
 This file is part of Abaclade.
 
 Abaclade is free software: you can redistribute it and/or modify it under the terms of the GNU
-General Public License as published by the Free Software Foundation, either version 3 of the
+Lesser General Public License as published by the Free Software Foundation, either version 3 of the
 License, or (at your option) any later version.
 
 Abaclade is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
-the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-Public License for more details.
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with Abaclade. If not, see
-<http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Lesser General Public License along with Abaclade. If
+not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------------------------*/
 
 #ifndef _ABACLADE_STD_TYPEINFO_HXX
