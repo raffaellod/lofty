25ffd16b049a1577d47c1a2b3a3f8796103ab1d9
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2016-01-20 19:31:24 -0500

    Add temporarily-empty abc::os::*registry*() test case

---
+++ test/abaclade/os.cxx
@@ -0,0 +1,39 @@
+﻿/* -*- coding: utf-8; mode: c++; tab-width: 3; indent-tabs-mode: nil -*-
+
+Copyright 2016 Raffaello D. Di Napoli
+
+This file is part of Abaclade.
+
+Abaclade is free software: you can redistribute it and/or modify it under the terms of the GNU
+Lesser General Public License as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+Abaclade is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with Abaclade. If
+not, see <http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------------------------------*/
+
+#include <abaclade.hxx>
+#include <abaclade/os.hxx>
+#include <abaclade/testing/test_case.hxx>
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#if ABC_HOST_API_WIN32
+
+namespace abc { namespace test {
+
+ABC_TESTING_TEST_CASE_FUNC(
+   os_registry,
+   "abc::os – accessing Windows Registry"
+) {
+   ABC_TRACE_FUNC(this);
+}
+
+}} //namespace abc::test
+
+#endif //if ABC_HOST_API_WIN32
