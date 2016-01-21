6005ba262ebd4dd34cdb253ac326991efc0764e0
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2016-01-20 19:12:44 -0500

    Add missing lambda variable capture

--- src/abaclade/process.cxx
+++ src/abaclade/process.cxx
@@ -227,7 +227,7 @@ bool env_var(str const & sName, str * psRet) {
       bRet = false;
    }
 #elif ABC_HOST_API_WIN32 //if ABC_HOST_API_POSIX
-   psRet->set_from([&bRet] (char_t * pch, std::size_t cchMax) -> std::size_t {
+   psRet->set_from([&csName, &bRet] (char_t * pch, std::size_t cchMax) -> std::size_t {
       /* ::GetEnvironmentVariable() returns < cchMax (length without NUL) if the buffer was large
       enough, or the required size (length including NUL) otherwise. */
       ::DWORD cchRet = ::GetEnvironmentVariable(csName, pch, static_cast< ::DWORD>(cchMax));
