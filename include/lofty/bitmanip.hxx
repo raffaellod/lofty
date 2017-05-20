6a9ebfee3e94a8513416ae06ca5ba52dde8081c3
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2017-05-20 00:16:34 -0400

    Re-license as LGPL 2.1 only

--- include/lofty/bitmanip.hxx
+++ include/lofty/bitmanip.hxx
@@ -4,16 +4,12 @@ Copyright 2010-2017 Raffaello D. Di Napoli
 
 This file is part of Lofty.
 
-Lofty is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
-Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
-option) any later version.
+Lofty is free software: you can redistribute it and/or modify it under the terms of version 2.1 of the GNU
+Lesser General Public License as published by the Free Software Foundation.
 
 Lofty is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
 more details.
-
-You should have received a copy of the GNU Lesser General Public License along with Lofty. If not, see
-<http://www.gnu.org/licenses/>.
 ------------------------------------------------------------------------------------------------------------*/
 
 #ifndef _LOFTY_BITMANIP_HXX
