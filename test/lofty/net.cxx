d20b94f0b731132b219aec8e26ee69e7689c1950
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2018-02-24 07:55:19 -0500

    Split IPv4/6 ip::address tests

--- test/lofty/net.cxx
+++ test/lofty/net.cxx
@@ -27,8 +27,8 @@ more details.
 namespace lofty { namespace test {
 
 LOFTY_TESTING_TEST_CASE_FUNC(
-   net_ip_address,
-   "lofty::net::ip::address – instantiation, display, and parsing"
+   net_ip_address_v4,
+   "lofty::net::ip::address – IPv4 instantiation, display, and parsing"
 ) {
    LOFTY_TRACE_FUNC();
 
@@ -36,16 +36,16 @@ LOFTY_TESTING_TEST_CASE_FUNC(
       static std::uint8_t const LOFTY_CPP_CAT(addr, i, _bytes)[] = __VA_ARGS__; \
       net::ip::address LOFTY_CPP_CAT(addr, i)(LOFTY_CPP_CAT(addr, i, _bytes)); \
       static char_t const LOFTY_CPP_CAT(addr, i, _str)[] = str;
-   ADDR(400, LOFTY_SL("0.0.0.0"),         { 0, 0, 0, 0 })
-   ADDR(440, LOFTY_SL("1.2.3.4"),         { 1, 2, 3, 4 })
-   ADDR(444, LOFTY_SL("255.255.255.255"), { 255, 255, 255, 255 })
+   ADDR(00, LOFTY_SL("0.0.0.0"),         { 0, 0, 0, 0 })
+   ADDR(40, LOFTY_SL("1.2.3.4"),         { 1, 2, 3, 4 })
+   ADDR(44, LOFTY_SL("255.255.255.255"), { 255, 255, 255, 255 })
    #undef ADDR
-   LOFTY_TESTING_ASSERT_EQUAL(addr400.version(), net::ip::version(net::ip::version::v4));
-   LOFTY_TESTING_ASSERT_EQUAL(addr440.version(), net::ip::version(net::ip::version::v4));
-   LOFTY_TESTING_ASSERT_EQUAL(addr444.version(), net::ip::version(net::ip::version::v4));
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr400), addr400_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr440), addr440_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr444), addr444_str);
+   LOFTY_TESTING_ASSERT_EQUAL(addr00.version(), net::ip::version(net::ip::version::v4));
+   LOFTY_TESTING_ASSERT_EQUAL(addr40.version(), net::ip::version(net::ip::version::v4));
+   LOFTY_TESTING_ASSERT_EQUAL(addr44.version(), net::ip::version(net::ip::version::v4));
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr00), addr00_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr40), addr40_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr44), addr44_str);
    LOFTY_TESTING_ASSERT_THROWS(text::syntax_error, from_str<net::ip::address>(LOFTY_SL("")));
    LOFTY_TESTING_ASSERT_THROWS(text::syntax_error, from_str<net::ip::address>(LOFTY_SL(".")));
    LOFTY_TESTING_ASSERT_THROWS(text::syntax_error, from_str<net::ip::address>(LOFTY_SL("0")));
@@ -65,60 +65,73 @@ LOFTY_TESTING_TEST_CASE_FUNC(
    ///LOFTY_TESTING_ASSERT_THROWS(text::syntax_error, from_str<net::ip::address>(LOFTY_SL("0.256.0.0")));
    ///LOFTY_TESTING_ASSERT_THROWS(text::syntax_error, from_str<net::ip::address>(LOFTY_SL("0.0.256.0")));
    ///LOFTY_TESTING_ASSERT_THROWS(text::syntax_error, from_str<net::ip::address>(LOFTY_SL("0.0.0.256")));
-   LOFTY_TESTING_ASSERT_EQUAL(from_str<net::ip::address>(addr400_str), addr400);
-   LOFTY_TESTING_ASSERT_EQUAL(from_str<net::ip::address>(addr440_str), addr440);
-   LOFTY_TESTING_ASSERT_EQUAL(from_str<net::ip::address>(addr444_str), addr444);
+   LOFTY_TESTING_ASSERT_EQUAL(from_str<net::ip::address>(addr00_str), addr00);
+   LOFTY_TESTING_ASSERT_EQUAL(from_str<net::ip::address>(addr40_str), addr40);
+   LOFTY_TESTING_ASSERT_EQUAL(from_str<net::ip::address>(addr44_str), addr44);
+}
+
+}} //namespace lofty::test
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace lofty { namespace test {
+
+LOFTY_TESTING_TEST_CASE_FUNC(
+   net_ip_address_v6,
+   "lofty::net::ip::address – IPv6 instantiation, display, and parsing"
+) {
+   LOFTY_TRACE_FUNC();
 
    #define ADDR(i, str, ...) \
       static std::uint8_t const LOFTY_CPP_CAT(addr, i, _bytes)[] = __VA_ARGS__; \
       net::ip::address LOFTY_CPP_CAT(addr, i)(LOFTY_CPP_CAT(addr, i, _bytes)); \
       static char_t const LOFTY_CPP_CAT(addr, i, _str)[] = str;
-   ADDR(600, LOFTY_SL("::"),            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })
-   ADDR(601, LOFTY_SL("1::"),           { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })
-   ADDR(602, LOFTY_SL("0:1::"),         { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })
-   ADDR(603, LOFTY_SL("0:0:1::"),       { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })
-   ADDR(604, LOFTY_SL("::1:0:0"),       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 })
-   ADDR(605, LOFTY_SL("::1:0"),         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 })
-   ADDR(606, LOFTY_SL("::1"),           { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 })
-   ADDR(607, LOFTY_SL("1::2"),          { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 })
-   ADDR(608, LOFTY_SL("0:1::2"),        { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 })
-   ADDR(609, LOFTY_SL("1::2:0"),        { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0 })
-   ADDR(610, LOFTY_SL("0:1::2:0"),      { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0 })
-   ADDR(611, LOFTY_SL("0:1::2:0:0"),    { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0 })
-   ADDR(612, LOFTY_SL("0:0:1::2:0"),    { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0 })
-   ADDR(613, LOFTY_SL("::1:0:0:2:0:0"), { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0 })
-   ADDR(614, LOFTY_SL("0:0:0:1::"),     { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 })
-   ADDR(615, LOFTY_SL("::1:0:0:0"),     { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 })
+   ADDR(00, LOFTY_SL("::"),            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })
+   ADDR(01, LOFTY_SL("1::"),           { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })
+   ADDR(02, LOFTY_SL("0:1::"),         { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })
+   ADDR(03, LOFTY_SL("0:0:1::"),       { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })
+   ADDR(04, LOFTY_SL("::1:0:0"),       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 })
+   ADDR(05, LOFTY_SL("::1:0"),         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 })
+   ADDR(06, LOFTY_SL("::1"),           { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 })
+   ADDR(07, LOFTY_SL("1::2"),          { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 })
+   ADDR(08, LOFTY_SL("0:1::2"),        { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 })
+   ADDR(09, LOFTY_SL("1::2:0"),        { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0 })
+   ADDR(10, LOFTY_SL("0:1::2:0"),      { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0 })
+   ADDR(11, LOFTY_SL("0:1::2:0:0"),    { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0 })
+   ADDR(12, LOFTY_SL("0:0:1::2:0"),    { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0 })
+   ADDR(13, LOFTY_SL("::1:0:0:2:0:0"), { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0 })
+   ADDR(14, LOFTY_SL("0:0:0:1::"),     { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 })
+   ADDR(15, LOFTY_SL("::1:0:0:0"),     { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 })
    ADDR(
-      660, LOFTY_SL("102:304:506:708:90a:b0c:d0e:f10"),
+      60, LOFTY_SL("102:304:506:708:90a:b0c:d0e:f10"),
       {   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15,  16 }
    )
    ADDR(
-      666, LOFTY_SL("ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"),
+      66, LOFTY_SL("ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"),
       { 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255 }
    )
    #undef ADDR
-   LOFTY_TESTING_ASSERT_EQUAL(addr600.version(), net::ip::version(net::ip::version::v6));
-   LOFTY_TESTING_ASSERT_EQUAL(addr660.version(), net::ip::version(net::ip::version::v6));
-   LOFTY_TESTING_ASSERT_EQUAL(addr666.version(), net::ip::version(net::ip::version::v6));
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr600), addr600_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr601), addr601_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr602), addr602_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr603), addr603_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr604), addr604_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr605), addr605_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr606), addr606_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr607), addr607_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr608), addr608_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr609), addr609_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr610), addr610_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr611), addr611_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr612), addr612_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr613), addr613_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr614), addr614_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr615), addr615_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr660), addr660_str);
-   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr666), addr666_str);
+   LOFTY_TESTING_ASSERT_EQUAL(addr00.version(), net::ip::version(net::ip::version::v6));
+   LOFTY_TESTING_ASSERT_EQUAL(addr60.version(), net::ip::version(net::ip::version::v6));
+   LOFTY_TESTING_ASSERT_EQUAL(addr66.version(), net::ip::version(net::ip::version::v6));
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr00), addr00_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr01), addr01_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr02), addr02_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr03), addr03_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr04), addr04_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr05), addr05_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr06), addr06_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr07), addr07_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr08), addr08_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr09), addr09_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr10), addr10_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr11), addr11_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr12), addr12_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr13), addr13_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr14), addr14_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr15), addr15_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr60), addr60_str);
+   LOFTY_TESTING_ASSERT_EQUAL(to_str(addr66), addr66_str);
 }
 
 }} //namespace lofty::test
